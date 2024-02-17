64421d55a8ee0fad3a5e5817a08a890f40a5500a
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-17 21:22:43 +0200

    Add create book

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 
+const bookService = require('../services/bookService');
 
+const { getErrorMessage } = require('../utils/errorUtils')
 
+router.get('/create', (req, res) => {
+    res.render('books/create');
+});
+
+router.post('/create', async (req, res) => {
+    const bookData = req.body;
+    bookData.owner = req.user._id;
+    try {
+        await bookService.create(bookData);
+        res.redirect('books/catalog');
+    } catch (error) {
+        res.render('books/create', { error: getErrorMessage(error), ...bookData });
+    }
+})
 
 
 module.exports = router;
